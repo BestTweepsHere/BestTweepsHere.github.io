06f778aaed2024e298883f2461c07ccc7d281c0e
Nandhini <nandhiniwebmail@gmail.com>
2018-12-18 08:07:17 +0530

    image updated and site removed

--- js/tamil.js
+++ js/tamil.js
@@ -53,9 +53,9 @@ $(document).ready(function() {
         profile_details	+=	'</div>';
         profile_details	+=	'<div class="buttons-div">';
 
-        if(website != "-"){
-            profile_details	+=	'<a href="'+website+'" target="_blank" class="button-bottom no-right-border w-button">Site</a>';
-        }
+        // if(website != "-"){
+        //     profile_details	+=	'<a href="'+website+'" target="_blank" class="button-bottom no-right-border w-button">Site</a>';
+        // }
 
         profile_details	+=	'<a href="'+handle_name+'" target="_blank" class="button-bottom right-button w-button">Twitter</a>';
         profile_details	+=	'</div>';
