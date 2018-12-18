6bff42c50b94a57be170208e81fea4480bf335a9
Nandhini <nandhiniwebmail@gmail.com>
2018-12-18 08:02:00 +0530

    test data added

---
+++ image/tamil.js
@@ -0,0 +1,14 @@
+
+test
+
+
+ profile_details	+=	'</div>';
+        profile_details	+=	'<div class="buttons-div">';
+
+        // if(website != "-"){
+        //     profile_details	+=	'<a href="'+website+'" target="_blank" class="button-bottom no-right-border w-button">Site</a>';
+        // }
+
+        profile_details	+=	'<a href="'+handle_name+'" target="_blank" class="button-bottom right-button w-button">Twitter</a>';
+        profile_details	+=	'</div>';
+		profile_details	+=	'</div>';
